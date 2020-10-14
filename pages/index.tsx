ec80df468389ace9da579fb70bc49a092df22f75
Daniel Trifunovic <daniel.tosaba@gmail.com>
2020-10-14 11:39:03 +0200

    project closure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import { Card, Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { motion } from "framer-motion";
-import { GetStaticProps } from "next";
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 import Link from "next/link";
 import React from "react";
@@ -17,46 +17,81 @@ const useStyles = makeStyles({
   },
 });
 
+const easing = [0.6, -0.05, 0.01, 0.99];
+const fadeInUp = {
+  initial: {
+    y: 60,
+    opacity: 0,
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.6,
+      ease: easing,
+    },
+  },
+};
+const stagger = {
+  animate: {
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+};
+
 export default function Home({ blankets }: HomeProps) {
   const classes = useStyles();
   return (
-    <motion.div exit={{ opacity: 0 }} initial={{opacity: 0}} animate={{opacity: 1}}>
+    <motion.div exit={{ opacity: 0 }} initial="initial" animate="animate">
       <div>
         <Head>
           <title>Framer Motion Transitions</title>
         </Head>
         <main>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <h1>CHOOSE YOUR BLANKET</h1>
-              </Grid>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <h1>CHOOSE YOUR BLANKET</h1>
+            </Grid>
+              <Grid container component={motion.div} variants={stagger} spacing={2}>
               {blankets.map((blanket) => (
                 <Grid key={blanket.id} item xs={12} sm={6} lg={4}>
-                  <Card>
-                    <Link href="/product/[id]" as={`/product/${blanket.id}`}>
-                      <a>
-                        <img
-                          src={blanket.imageUrl.cover}
-                          alt={blanket.name}
-                          className={classes.image}
-                        />
-                      </a>
-                    </Link>
-                  </Card>
+                  <motion.div variants={fadeInUp}>
+                    <Card>
+                      <Link href="/product/[id]" as={`/product/${blanket.id}`}>
+                        <a>
+                          <motion.img 
+                          initial={{x:60, opacity:0}} animate={{x:0, opacity:1}} transition={{delay:.3}}
+                            src={blanket.imageUrl.cover}
+                            alt={blanket.name}
+                            className={classes.image}
+                          />
+                        </a>
+                      </Link>
+                    </Card>
+                  </motion.div>
                 </Grid>
               ))}
-            </Grid>
+              </Grid>
+          </Grid>
         </main>
       </div>
     </motion.div>
   );
 }
 
-export const getStaticProps: GetStaticProps = async (ctx) => {
-  const response = await fetch(
-    "http://localhost:4001/blankets/"
-  );
+// export const getStaticProps: GetStaticProps = async (ctx) => {
+//   const response = await fetch("http://localhost:4001/blankets/");
+//   const blankets = await response.json();
+//   return {
+//     props: { blankets },
+//   };
+// };
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await fetch("http://localhost:4001/blankets/");
   const blankets = await response.json();
+
   return {
     props: { blankets },
   };
--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -2,15 +2,20 @@ import { Card, Grid, Paper, Typography } from "@material-ui/core";
 import CardContent from "@material-ui/core/CardContent";
 import { motion } from "framer-motion";
 import fetch from "isomorphic-unfetch";
-import { GetStaticPaths, GetStaticProps } from "next";
+import { GetServerSideProps } from "next";
 import ImageGallery from "react-image-gallery";
 import { Blanket } from "../../models/blanket";
 import "../../node_modules/react-image-gallery/styles/css/image-gallery.css";
 
 type ProductDetailsProps = Blanket;
 
-export default function ProductDetails({ name, description, imageUrl, size, price }: ProductDetailsProps) {
-
+export default function ProductDetails({
+  name,
+  description,
+  imageUrl,
+  size,
+  price,
+}: ProductDetailsProps) {
   let images = [];
   imageUrl.url.forEach((img) => {
     images.push({
@@ -25,55 +30,65 @@ export default function ProductDetails({ name, description, imageUrl, size, pric
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
     >
-        <Grid container spacing={2}>
-          <Grid item xs={12} sm={6}>
-            <ImageGallery items={images} />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <Paper elevation={0}>
-              <Card>
-                <CardContent>
-                  <Typography variant="h4" gutterBottom>
-                    {name}
-                  </Typography>
-                  <Typography variant="subtitle1">
-                    Price: {price.small}
-                  </Typography>
-                  <Typography variant="body1" gutterBottom>
-                    {description}
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Paper>
-          </Grid>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6} component={motion.div} initial={{opacity:0, x:30}} animate={{opacity:1, x:0}} transition={{delay:.2}}>
+          <ImageGallery items={images} />
         </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper elevation={0}>
+            <Card>
+              <CardContent>
+                <Typography variant="h4" gutterBottom>
+                  {name}
+                </Typography>
+                <Typography variant="subtitle1">
+                  Price: {price.small}
+                </Typography>
+                <Typography variant="body1" gutterBottom>
+                  {description}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Paper>
+        </Grid>
+      </Grid>
     </motion.div>
   );
 }
 
-export const getStaticProps: GetStaticProps = async (context) => {
-  const id = context.params.id;
-  const response = await fetch(
-    `http://localhost:4001/blankets/${id}`
-  );
+// export const getStaticProps: GetStaticProps = async (context) => {
+//   const id = context.params.id;
+//   const response = await fetch(
+//     `http://localhost:4001/blankets/${id}`
+//   );
+//   const blanket = await response.json();
+
+//   return {
+//     props: blanket,
+//   };
+// };
+
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   const response = await fetch(
+//     "http://localhost:4001/blankets/"
+//   );
+//   const blankets: Blanket[] = await response.json();
+//   const paths = blankets.map((blanket) => {
+//     return { params: { id: blanket.id.toString() } };
+//   });
+
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// };
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { id } = context.params;
+  const response = await fetch(`http://localhost:4001/blankets/${id}`);
   const blanket = await response.json();
 
   return {
     props: blanket,
-  };
-};
-
-export const getStaticPaths: GetStaticPaths = async () => {
-  const response = await fetch(
-    "http://localhost:4001/blankets/"
-  );
-  const blankets: Blanket[] = await response.json();
-  const paths = blankets.map((blanket) => {
-    return { params: { id: blanket.id.toString() } };
-  });
-
-  return {
-    paths,
-    fallback: false,
   };
 };
